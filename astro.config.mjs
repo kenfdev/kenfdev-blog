c9f75506384f59494c6523313142efc8997677b4
Ken Fukuyama <kenfdev@gmail.com>
2026-01-02 21:49:06 +0900

    Update site URL in astro.config.mjs and add TypeScript as a dev dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com', // 本番ドメインに変更してください
+  site: 'https://blog.kenev.net', // 本番ドメインに変更してください
   output: 'static',
   trailingSlash: 'always',
   build: {
